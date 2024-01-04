0acd9395bdc4be600b1a18ac621931a4c76c53c4
Zoltan Marcsek <hello@marcsekz.me>
2024-01-04 09:36:12 +0100

    Resize dialog elements for consistency

--- CopperThiefDlg.py
+++ CopperThiefDlg.py
@@ -31,7 +31,7 @@ class CopperThiefDlg(Dialog):
         self.m_labelSep = StaticText(self, ID_ANY, u"Dot Separation  (mm)  ", DefaultPosition, DefaultSize, 0)
         self.m_labelSep.Wrap(-1)
         self.m_spacing = TextCtrl(self, ID_ANY, u"2", DefaultPosition, DefaultSize, 0)
-        self.m_spacing.SetMinSize(Size(1000, -1))
+        self.m_spacing.SetMinSize(Size(200, -1))
 
         bSizerSep.Add(self.m_labelSep, 1, ALL | EXPAND, 5)
         bSizerSep.Add(self.m_spacing, 1, ALL, 5)
@@ -44,7 +44,7 @@ class CopperThiefDlg(Dialog):
         self.m_labelRad = StaticText(self, ID_ANY, u"Dot Radius  (mm)  ", DefaultPosition, DefaultSize, 0)
         self.m_labelRad.Wrap(-1)
         self.m_radius = TextCtrl(self, ID_ANY, u"0.5", DefaultPosition, DefaultSize, 0)
-        self.m_radius.SetMinSize(Size(1000, -1))
+        self.m_radius.SetMinSize(Size(200, -1))
 
         bSizerRad.Add(self.m_labelRad, 1, ALL | EXPAND, 5)
         bSizerRad.Add(self.m_radius, 1, ALL, 5)
@@ -57,7 +57,7 @@ class CopperThiefDlg(Dialog):
         self.m_labelClearance = StaticText(self, ID_ANY, u"Clearance multiplier  ", DefaultPosition, DefaultSize, 0)
         self.m_labelClearance.Wrap(-1)
         self.m_clearance = TextCtrl(self, ID_ANY, u"3", DefaultPosition, DefaultSize, 0)
-        self.m_clearance.SetMinSize(Size(1000, -1))
+        self.m_clearance.SetMinSize(Size(200, -1))
 
         bSizerClearance.Add(self.m_labelClearance, 1, ALL | EXPAND, 5)
         bSizerClearance.Add(self.m_clearance, 1, ALL, 5)
@@ -72,7 +72,7 @@ class CopperThiefDlg(Dialog):
 
         self.m_pattern_list = [ u"Squares", u"Dots in square grid", u"Dots in triangular grid"]
         self.m_pattern = ComboBox( self, ID_ANY, u"Squares", DefaultPosition, DefaultSize, self.m_pattern_list)
-        self.m_pattern.SetMinSize(Size(1000, -1))
+        self.m_pattern.SetMinSize(Size(200, -1))
 
         bSizerPattern.Add(self.m_labelPattern, 1, ALL | EXPAND, 5)
         bSizerPattern.Add(self.m_pattern, 1, ALL, 5)
@@ -85,7 +85,7 @@ class CopperThiefDlg(Dialog):
         self.m_labelCleanup.Wrap(-1)
         
         self.m_cleanup = CheckBox(self)
-        self.m_cleanup.SetMinSize(Size(1000, -1))
+        self.m_cleanup.SetMinSize(Size(200, -1))
         
         bSizerCleanup.Add(self.m_labelCleanup, 1, ALL | EXPAND, 5)
         bSizerCleanup.Add(self.m_cleanup, 1, ALL, 5)
